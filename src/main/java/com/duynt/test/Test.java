b99487118626dc081117aa538a79503a0630ee3c
duynt <duynt@ominext.local>
2021-12-22 11:04:29 +0700

    up

--- src/main/java/com/duynt/test/Test.java
+++ src/main/java/com/duynt/test/Test.java
@@ -1,5 +1,6 @@
 package com.duynt.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Test {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/test")
     public ResponseEntity helloWorld() {
-        return ResponseEntity.ok("hello world!");
+        return ResponseEntity.ok(userRepository.findAll());
     }
 }
